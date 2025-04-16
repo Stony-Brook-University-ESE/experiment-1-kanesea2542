a0c9c157469dca3cdecdcdae87e48dcd7506cc0e
kanesea2542 <kanesea2542@gmail.com>
2025-04-16 13:08:05 -0400

    Final

--- Build/Experiment1.c
+++ Build/Experiment1.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h> // Added ctype.h for tolower function
 
 int main() {
-    char str[8];
+    char str[100]; // Modified the string length to account for larger character words
     printf("Enter a word: ");
-    scanf("%s", str);
-
+    scanf("%s", str); 
+    
     int len = strlen(str);
     int isPalindrome = 1;
-
+   
     for (int i = 0; i < len / 2; i++) {
-        if (str[i] != str[len - 1 - i]) {
-            isPalindrome = 0;
+        if (tolower(str[i]) != tolower(str[len - 1 - i])) { // Added tolower function, account for different upper/lowcased cases
+            isPalindrome = 0;       
             break;
         }
     }
